b691df64ba0f13c415aa29afccfa99633005f18c
Faisalwich <wicahyo45@gmail.com>
2026-01-03 21:17:14 +0700

    Update supabase client pakai env variable

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://nhicngtwiokccasqisay.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oaWNuZ3R3aW9rY2Nhc3Fpc2F5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwOTczMjEsImV4cCI6MjA4MjY3MzMyMX0.6t47HPGNwEtlrcWey_6xRKMqY6N_MiXDztGfDeXDvb4";
+// 👇 Perhatikan bagian ini!
+// Kita tidak lagi menulis link panjang di sini, tapi memanggil dari .env
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
